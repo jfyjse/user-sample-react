8f15d6d22e51771eefdaf1310fb2c37839e11edb
Joffy Jose <joffy.jose.jfy@gmail.com>
2022-01-13 17:42:45 +0530

    states set

--- src/Components/User/AddUsers.js
+++ src/Components/User/AddUsers.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React ,{useState}from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const Adduser = (props) => {
-  const addUserHandler = (event) => {
-    event.preventDefault();
-  };
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+  
+    const addUserHandler = (event) => {
+      event.preventDefault();
+      console.log(enteredName, enteredAge);
+    };
+  
+    const nameChangeHandler = (event) => {
+      setEnteredName(event.target.value);
+    };
+  
+    const ageChangeHandler = (event) => {
+      setEnteredAge(event.target.value);
+    };
   return (
     <Card className ={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text"></input>
+        <input onChange={nameChangeHandler} id="name" type="text"></input>
         <label htmlFor="age">Age</label>
-        <input id="age" type="number"></input>
+        <input onChange={ageChangeHandler} id="age" type="number"></input>
         <Button type = "submit">Click to Add</Button>
       </form>
       </Card>
