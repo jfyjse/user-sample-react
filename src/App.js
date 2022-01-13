3e197a1f29a6618d0ccd6a9ba9ae3c8d8e137d87
Joffy Jose <joffy.jose.jfy@gmail.com>
2022-01-13 18:22:52 +0530

    users list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Adduser from "./Components/User/AddUsers";
+import UsersList from "./Components/User/UsersList";
 
 function App() {
   return (
     <div>
       <Adduser></Adduser>
+      <UsersList users ={[]}></UsersList>
     </div>
   );
 }
