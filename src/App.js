271b1414e2ba71ce090098728610e0638ff86785
Joffy Jose <joffy.jose.jfy@gmail.com>
2022-01-14 17:25:04 +0530

    add to list minor changes in code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
-import Adduser from "./Components/User/AddUsers";
-import UsersList from "./Components/User/UsersList";
+import React, { useState } from 'react';
+
+import AddUser from './Components/User/AddUser';
+import UsersList from './Components/User/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <Adduser></Adduser>
-      <UsersList users ={[]}></UsersList>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
